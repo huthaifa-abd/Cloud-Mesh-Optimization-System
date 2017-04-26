b84adc86e176af75c16e8ec332fc0592ce3a2255
huthaifa-abd <huth2004@gmail.com>
2017-04-26 09:56:36 +0300

    [HTML] Layout Update

--- Source/CloudDecimationSystem/WebApplication/WebApplication/Default.aspx.designer.cs
+++ Source/CloudDecimationSystem/WebApplication/WebApplication/Default.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace WebApplication {
         protected global::System.Web.UI.WebControls.Label labOptPolyCount;
         
         /// <summary>
+        /// labResultNotification control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label labResultNotification;
+        
+        /// <summary>
         /// Panel1 control.
         /// </summary>
         /// <remarks>
